2facb80e426d59ce263986847f0cff8ec2c3d751
jasonxiaohan <jasonxiaohan198@qq.com>
2019-12-24 17:32:23 +0800

    添加微信分账功能

--- src/controller/profit.js
+++ src/controller/profit.js
@@ -1,9 +1,7 @@
 /**
 
- @Name： 扫码人员管理 
+ @Name： 分账人员管理 
  */
-
-
 layui.define(['table', 'form'], function(exports){
   var $ = layui.$
   ,admin = layui.admin
@@ -13,24 +11,55 @@ layui.define(['table', 'form'], function(exports){
   ,util = layui.util
   ,form = layui.form;
 
-  //网红项目管理
+  //分账人员管理
   table.render({
-    elem: '#LAY-scan-manage'
-    ,url: setter.remoteurl+'/scan/scans' //模拟接口
+    elem: '#LAY-profit-manage'
+    ,url: setter.remoteurl+'/profit/profits' //模拟接口
     ,where: {
       access_token: layui.data(setter.tableName).access_token,
     }
     ,cols: [[
     {type: 'checkbox', fixed: 'left'}
-      // ,{field: 'scanId', width: 100, title: 'ID', sort: true}
-      ,{field: 'username', title: '姓名', width: 250}
-      ,{field: 'cellphone', title: '手机号', width: 350}
-      ,{field: 'merchname', title: '商户名称', width: 350}
+      // ,{field: 'profitId', width: 100, title: 'ID', sort: true}
+      ,{field: 'type', title: '接收方类型', width: 150, templet:function(d){
+        if(d.type == 'MERCHANT_ID') {
+          return '商户ID';
+        } else if(d.type == 'PERSONAL_WECHATID') {
+          return '个人微信';
+        } 
+        return '个人openid';
+      }}
+      ,{field: 'name', title: '姓名', width: 160}
+      ,{field: 'account', title: '账号', width: 170}
+      ,{field: 'relation_type', title: '关系类型', width: 200, templet:function(d) {
+        if(d == 'SERVICE_PROVIDER') {
+          return '服务商';
+        }else if(d.relation_type == 'STORE') {
+          return '门店';
+        }else if(d.relation_type == 'STAFF') {
+          return '员工';
+        }else if(d.relation_type == 'STORE_OWNER') {
+          return '店主';
+        }else if(d.relation_type == 'PARTNER') {
+          return '合作伙伴';
+        }else if(d.relation_type == 'HEADQUARTER') {
+          return '总部';
+        } else if(d.relation_type == 'BRAND') {
+          return '品牌方';
+        }else if(d.relation_type == 'DISTRIBUTOR') {
+          return '分销商';
+        }else if(d.relation_type == 'USER') {
+          return '用户';
+        }else if(d.relation_type == 'SUPPLIER') {
+          return '供应商';
+        }
+      }}
+      ,{field: 'merchname', title: '商户名称', width: 300}
       ,{field: 'enabled', title: '状态',templet: '#buttonTpl', width: 200, align: 'center'}
-      ,{field: 'time', title: '添加时间',templet: '#buttonTpl', width: 250, align: 'center', templet:function(d){
+      ,{field: 'time', title: '添加时间',templet: '#buttonTpl', align: 'center', templet:function(d){
         return util.toDateString(d.time, "yyyy-MM-dd HH:mm:ss");
       }}
-      ,{title: '操作', align:'center', fixed: 'right',width: 150, toolbar: '#table-scan-webuser'}
+      ,{title: '操作', align:'center', fixed: 'right',width: 150, toolbar: '#table-profit-webuser'}
     ]]
     ,page: true
     ,limit: 30
@@ -41,7 +70,7 @@ layui.define(['table', 'form'], function(exports){
   });
   
   //监听工具条
-  table.on('tool(LAY-scan-manage)', function(obj){
+  table.on('tool(LAY-profit-manage)', function(obj){
     var data = obj.data;
     if(obj.event === 'del'){
       layer.prompt({
@@ -52,9 +81,9 @@ layui.define(['table', 'form'], function(exports){
         
         layer.confirm('真的删除行么', function(index){
           admin.req({
-            url: setter.remoteurl+'/scan/scans'
+            url: setter.remoteurl+'/profit/profits'
             ,method: 'DELETE'
-            ,data: {scanId: data.scanId}
+            ,data: {profitId: data.profitId}
             ,success: function(res){
               if (res.code == 0) {
                 layer.msg("删除成功",{time: 1000,icon: 1},function(){
@@ -73,21 +102,21 @@ layui.define(['table', 'form'], function(exports){
       });
     } else if(obj.event === 'edit'){
       admin.popup({
-        title: '编辑扫码人员'
-        ,area: ['420px', '450px']
-        ,id: 'LAY-popup-scan-edit'
+        title: '编辑分账人员'
+        ,area: ['450px', '550px']
+        ,id: 'LAY-popup-profit-edit'
         ,success: function(layero, index){
-          view(this.id).render('set/system/scan', data).done(function(){
-            form.render(null, 'layuiadmin-form-scan');
+          view(this.id).render('set/system/profit', data).done(function(){
+            form.render(null, 'layuiadmin-form-profit');
             
             //监听提交
-            form.on('submit(LAY-scan-back-submit)', function(data){
+            form.on('submit(LAY-profit-back-submit)', function(data){
               var field = data.field; //获取提交的字段
 
               //提交 Ajax 成功后，关闭当前弹层并重载表格
               //$.ajax({});
               admin.req({
-                url: setter.remoteurl+'/scan/scans'
+                url: setter.remoteurl+'/profit/profits'
                 ,method: 'PUT'
                 ,data: field
                 ,success: function(res){
@@ -103,7 +132,7 @@ layui.define(['table', 'form'], function(exports){
                 }
               }); 
 
-              layui.table.reload('LAY-scan-manage'); //重载表格
+              layui.table.reload('LAY-profit-manage'); //重载表格
               layer.close(index); //执行关闭 
             });
           });
@@ -115,5 +144,5 @@ layui.define(['table', 'form'], function(exports){
   });
 
 
-  exports('scan', {})
+  exports('profit', {})
 });
